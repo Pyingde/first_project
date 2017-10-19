d6cddd8e8c393c5cb4a2973fe8fa3ba309719a73
CSX <251156539@qq.com>
2017-10-19 14:24:43 +0800

    小修改

--- src/js/TBload.js
+++ src/js/TBload.js
@@ -1,20 +1,6 @@
 define(['jquery'],function($){
     return ()=>{
-<<<<<<< HEAD
         $('#Header').load('html/Header.html');
         $('#Lnav').load('html/Lnav.html');
-=======
-        $('#Header').load('./Header.html');
-        $('#Lnav').load('./Lnav.html');
->>>>>>> f59184d171a046275db93a1124537868c944deee
     }
-
-    // return [$('#Header').load('./Header.html'),$('#Lnav').load('./Lnav.html')]
-        // if(t === 'index'){
-        //     return [$('#indexTop').load('html/indexTop.html'),$('#indexBottom').load('html/indexBottom.html')]
-        // }else{
-        //     return [$('#indexTop').load('../html/indexTop2.html'),$('#indexBottom').load('../html/indexBottom2.html')]
-        // }
-    
-     
 })
--- src/js/sets.js
+++ src/js/sets.js
@@ -1,31 +1,18 @@
 define(['jquery'],function($){
     return ()=>{
-<<<<<<< HEAD
         // 先查询权限
         $('.lnav li').click(function(){
             if($(this).index()>0){
-            $(this).next().slideToggle()
-            if($(this).find('.xia').html() === 'ν'){
-                $(this).find('.xia').html('>')
-            }else{
-                $(this).find('.xia').html('ν')
+                $(this).next().slideToggle()
+
+                if($(this).find('.xia').html() === 'ν'){
+
+                    $(this).find('.xia').html('>')
+                }else{
+
+                    $(this).find('.xia').html('ν')
+                }
             }
-            }
-=======
-    	console.log(44);
-        // 先查询权限
-        $('.lnav li').click(function(){
-        	if($(this).index()>0){
-        		 $(this).next().slideToggle()
-	            if($(this).find('.xia').html() === 'ν'){
-	                $(this).find('.xia').html('>')
-	            }else{
-	                $(this).find('.xia').html('ν')
-	            }
-        	}
-           
->>>>>>> f59184d171a046275db93a1124537868c944deee
-            
-        })
+        });
     }
 })
